8d6154445ecdaceb710f9e12274a0dd4efb3ad73
Akua Apeagyei <apeagyei.akua@gmail.com>
2019-04-04 16:01:31 +0100

    Created feature test for alternative outcome where it is not the users birth. TBC

--- lib/countdown.rb
+++ lib/countdown.rb
@@ -10,13 +10,14 @@ class Countdown
 
   def  days_until_birthday
 
-    if @month < Date.today.month
+    if @month.to_i < Date.today.month
       next_bday = "#{@day}-#{@month}-#{Date.today.year + 1}"
     else
       next_bday = "#{@day}-#{@month}-#{Date.today.year}"
     end
     (Date.parse(next_bday) - Date.today).to_i
   end
+
 
   attr_reader :day, :month, :name
 end
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -7,4 +7,13 @@ feature 'Enter birthday' do
     click_button 'Go'
     expect(page).to have_content 'Happy Birthday Akua'
   end
+
+  scenario 'submits day and month' do
+    visit ('/')
+    fill_in :name, with: 'Akua'
+    fill_in :day, with: '05'
+    fill_in :month, with: '06'
+    click_button 'Go'
+    expect(page).to have_content 'You have x amount of days till your birthday'
+  end
 end
